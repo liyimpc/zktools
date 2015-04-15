afe85c551e74f5039dd7c1c18717a4e66e2c99be
guangming <guangming@guangmingdeMacBook-Pro-2.local>
2015-04-15 16:46:39 +0800

    add java doc

--- src/main/java/org/mogujie/zookeeper/tools/SuperUser.java
+++ src/main/java/org/mogujie/zookeeper/tools/SuperUser.java
@@ -1,3 +1,12 @@
+/*
+ * 蘑菇街 Inc.
+ * Copyright (c) 2010-2013 All Rights Reserved.
+ *
+ * Author     :guangming
+ * Version    :1.0
+ * Create Date:2015年4月15日
+ */
+
 package org.mogujie.zookeeper.tools;
 
 import java.io.IOException;
